343320b7ea948a94aa2f189be12f132bd7fb646e
Yusuf-cm <yusufngetich7@gmail.com>
2025-09-18 09:03:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const authRoutes = require('./routes/auth.routes');
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to Ronohs Decor Shop API!');
